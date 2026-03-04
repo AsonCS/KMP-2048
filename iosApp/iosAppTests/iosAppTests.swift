836a5f54bc9885df59d1b58a9c2738a5d64b8973
AsonCS <acsgsa@hotmail.com>
2026-03-04 17:48:36 -0300

    iOS files

---
+++ iosApp/iosAppTests/iosAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppTests.swift
+//  iosAppTests
+//
+//  Created by Anderson Costa da Silva on 04/03/26.
+//
+
+import Testing
+@testable import iosApp
+
+struct iosAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
