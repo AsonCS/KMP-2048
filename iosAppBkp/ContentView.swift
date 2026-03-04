edb6d0d426cb8d4d756db683328f4271019d7ac0
AsonCS <acsgsa@hotmail.com>
2026-03-04 16:28:42 -0300

    Running on iOS

---
+++ iosAppBkp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Anderson Costa da Silva on 04/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosAppBkp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Anderson Costa da Silva on 04/03/26.
+//
+
+import SwiftUI
+
+// @main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
